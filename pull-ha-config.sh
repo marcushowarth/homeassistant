10671cef7676bab1a9ca244b91097a21141a4386
Marcus Howarth <info@howarth.eu>
2024-02-14 04:43:44 +0000

    Initial commit

---
+++ pull-ha-config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# theses are definitely updated through the UI.. didn't want to copy configuration though as more likely editing locally
+scp marcus@farmington:/home/hauser/automations.yaml .
+scp marcus@farmington:/home/hauser/scenes.yaml .
+scp marcus@farmington:/home/hauser/scripts.yaml .
+scp marcus@farmington:/home/hauser/secrets.yaml .
+
+# why need this?
+#scp marcus@farmington:/home/hauser/entities/input_number entities/input_number
