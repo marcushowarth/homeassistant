c7da3b20c4a80a702e66acc30aafa0d95355a276
Marcus Howarth <info@howarth.eu>
2024-08-21 03:28:47 +0100

    deployment script changes

--- deploy-ha-config.sh
+++ deploy-ha-config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 scp configuration.yaml marcus@farmington:/home/hauser
+scp secrets.yaml marcus@farmington:/home/hauser
 
 # no longer used, but there for info
 #scp entities/input_number/*.yaml marcus@farmington:/home/hauser/entities/input_number
---
+++ pull-ha-configuration.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# theses are definitely updated through the UI..
+scp marcus@farmington:/home/hauser/configuration.yaml .
+
+# use pull-ha-configuration.sh to copy configuration over if edited directly/outside here
+
+# why need this?
+#scp marcus@farmington:/home/hauser/entities/input_number entities/input_number
