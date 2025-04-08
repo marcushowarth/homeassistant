b58476f2e809cebdc58e7549f7168290140eb676
Marcus Howarth <marcus@howarth.eu>
2025-04-08 13:10:10 +0100

    new templates config for the Gas Meter template sensor using the Octopus integration

--- deploy-ha-config.sh
+++ deploy-ha-config.sh
@@ -7,7 +7,7 @@ scp docker-compose.yaml marcus@farmington:/home/hauser
 scp configuration.yaml marcus@farmington:/home/hauser
 scp secrets.yaml marcus@farmington:/home/hauser
 scp mqtt.yaml marcus@farmington:/home/hauser
-
+scp templates.yaml marcus@farmington:/home/hauser
 
 
 # no longer used, but there for info
--- pull-ha-config.sh
+++ pull-ha-config.sh
@@ -6,7 +6,7 @@ scp marcus@farmington:/home/hauser/scenes.yaml .
 scp marcus@farmington:/home/hauser/scripts.yaml .
 scp marcus@farmington:/home/hauser/secrets.yaml .
 scp marcus@farmington:/home/hauser/mqtt.yaml .
-
+scp marcus@farmington:/home/hauser/templates.yaml .
 
 # why need this?
 #scp marcus@farmington:/home/hauser/entities/input_number entities/input_number
