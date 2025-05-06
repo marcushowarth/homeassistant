66596e894b0c633aa21569e9dcf03481ee266ae0
Marcus Howarth <marcus@howarth.eu>
2025-05-06 19:31:18 +0100

    Backup Zigbee makes file permission for read for crontab user

--- backupZigbee.sh
+++ backupZigbee.sh
@@ -4,6 +4,10 @@ set -e
 BACKUP_DIR="/home/hauser/backups"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
+# Ensure all files to be backed up are readable by cronjob
+sudo find mosquitto zigbee2mqtt-data -type d -exec chmod o+rx {} +
+sudo find mosquitto zigbee2mqtt-data -type f -exec chmod o+r {} +
+
 # Backup Mosquitto
 tar czf "$BACKUP_DIR/mosquitto_$TIMESTAMP.tar.gz" mosquitto/
 
